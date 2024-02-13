7782cec091681947bd0a1be072fdf0bcbd99eb5c
sandroservo <sandro_servo@hotmail.com>
2024-02-13 19:57:05 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,18 @@
-import Header from "../_components/header";
 
+import Header from "../_components/header";
+import {format} from "date-fns"
+import { ptBR } from "date-fns/locale";
 export default function Home() {
   return (
-    <Header />
+    <div>
+       <Header />
+       <div className="px-5 pt-5">
+    <h2 className="text-xl font-bold">Olá, Sandro!</h2>
+    <p className="capitalize">{format(new Date(), "EEEE',' dd 'de' MMMM",{
+      locale:ptBR
+    })}</p>
+    </div>
+    </div>
+   
   );
 }
