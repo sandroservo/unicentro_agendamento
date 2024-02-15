1e427e18a4ff7e097a78b0ea25c6997df498ec8c
sandroservo <sandro_servo@hotmail.com>
2024-02-15 00:43:10 -0300

    feat: add sign and sign out

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,21 +1,84 @@
 "use client"
 
 import Image from "next/image"
-import { Card, CardContent } from "./ui/card"
+import { Card, CardContent, } from "./ui/card"
+import { Avatar, AvatarImage } from "./ui/avatar"
 import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
+import { CalendarCheck2Icon, HomeIcon, HopIcon, LogInIcon, MenuIcon, UserCheck } from "lucide-react";
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
+import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+
 
 
 const Header = () => {
-    
+    const { data, status } = useSession()
+    const handlerLogoutClick = () => signOut()
+    const handlerLoginClick = () => signIn("google")
     return (
         <Card>
             <CardContent className="p-5  justify-between items-center flex flex-row">
                 <Image src="/logo.png" alt="logo agendlabs" height={22} width={180} />
-                <Button variant="outline" size="icon" className="h-8 w-8" >
-                    <MenuIcon size={18}/>
-                </Button >
-                
+                <Sheet>
+                    <SheetTrigger asChild>
+                        <Button variant="outline" size="icon" className="h-8 w-8" >
+                            <MenuIcon size={18} />
+                        </Button >
+                    </SheetTrigger>
+                    <SheetContent className="p-0">
+                        <SheetHeader className="text-left border-b border-solid border-secundary p-5">
+                            <SheetTitle>
+                                Menu
+                            </SheetTitle>
+                        </SheetHeader>
+
+                        {data?.user ? (
+                            <div className="flex justify-between px-5 py-6 items-center">
+                                <div className="flex items-center gap-3 ">
+                                    <Avatar>
+                                        <AvatarImage src={data.user?.image ?? ""} />
+                                    </Avatar>
+
+                                    <h2 className="font-bold">{data.user.name}</h2>
+                                </div>
+
+                                <Button onClick={handlerLogoutClick} variant="secondary" size="icon">
+                                    <LogInIcon />
+                                </Button>
+                            </div>
+                        ) : (
+
+                            <div className="flex flex-col px-5 py-6 gap-3">
+                                <div className="flex items-center gap-3">
+                                    <UserCheck size={32} />
+                                    <h2 className="focnt-bold">Olá, faça seu login!</h2>
+                                </div>
+                                <Button onClick={handlerLoginClick} variant="secondary" className="w-full" >
+                                    <LogInIcon className="mr-2" size={18} />
+                                    Fazer Login
+                                </Button>
+                            </div>
+                        )}
+
+                        <div className="flex flex-col gap-3 px-5">
+                            <Button variant="outline" className="justify-start" asChild>
+                                <Link href="/">
+                                    <HomeIcon size={18} className=" mr-2 " />
+                                    Início
+                                </Link>
+                            </Button>
+                            {data?.user && (
+                                <Button variant="outline" className="justify-start" asChild>
+                                    <Link href="/bookings">
+                                        <CalendarCheck2Icon size={18} className=" mr-2 " />
+                                        Agendamentos
+                                    </Link>
+                                </Button>
+                            )}
+                        </div>
+                    </SheetContent>
+                </Sheet>
+
             </CardContent>
         </Card>
     )
